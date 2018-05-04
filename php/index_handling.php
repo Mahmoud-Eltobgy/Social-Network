a947760645edfc4d2fda74d1aa61c506f9c3d47e
Mahmoud <15x0069@eng.asu.edu.eg>
2018-05-04 09:53:11 +0200

    Add files via upload
    updated to save users comments in our database

--- php/index_handling.php
+++ php/index_handling.php
@@ -1,29 +1,33 @@
 <?php
-
 //Redirecting to the register page if the value isset($_SESSION['email']) is not set
 //We set that value when the login is successful
-require 'php/classes/post.php';
+
+
+require 'php/classes/Post.php';
 if(!isset($_SESSION['email'])){
     header("Location: register.php");    
 }
-
 if(isset($_POST['logout'])){
     session_destroy();
     header("Location: register.php");
 }
 if(isset($_POST['Post']))
 {
-	//update values in the user table
-	$num_posts=$user_info['num_posts'];
-	$num_posts+=1;
-	$current_user->update("num_posts",$num_posts);
-	//we need to creat a new post in the table posts
-	$post_value=$_POST['post_value'];
-	$new_post=new post($user_info['id']);
-	$post_data=array('','',$post_value,'','','');
-	$new_post->add($post_data);
+	//Create a new post in the table posts
+	$new_post=new Post($current_user_info['id']);
+	$new_post->add($_POST['post_value']);
 	//refresh user posts query in the header page 
-	$user_posts=$current_user->getAllPosts();
+	$current_user_posts=$current_user->getAllPosts();
 	//refresh the page to view the posts
+	header("Location: index.php");
+}
+
+if(isset($_POST['comment']))
+{
+	$post_id=$_POST['post_id'];
+	$user_id=$_SESSION['id'];
+	$newComment=new Comment($user_id,$post_id);
+	$newComment->add($_POST['comment_value']);
+	//refresh the page to view new comments
 	header("Location: index.php");
 }
